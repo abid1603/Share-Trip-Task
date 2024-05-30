4c20cfe5ab0637dfc694bc5f163a0879a55699b4
Abidur Rahman Abid <88293639+abid1603@users.noreply.github.com>
2024-05-30 18:29:15 +0600

    Build issue solved

--- src/Components/BreadCum.js
+++ src/Components/BreadCum.js
@@ -5,11 +5,11 @@ export default function BreadCum() {
   return (
     <div className='flex my-4 items-center max-lg:justify-center'>
       <span className='text-[13px] text-[#5A6573]'>Home</span>
-      <img src={angleIcon} className='ml-[6px]'></img>
+      <img src={angleIcon} alt='angleIcon' className='ml-[6px]'></img>
       <span className='text-[13px] text-[#5A6573] ml-[6px]'>Booking History</span>
-      <img src={angleIcon} className='ml-[6px]'></img>
+      <img src={angleIcon} alt='angleIcon' className='ml-[6px]'></img>
       <span className='text-[13px] text-[#5A6573] ml-[6px]'>Flight</span>
-      <img src={angleIcon} className='ml-[6px]'></img>
+      <img src={angleIcon} alt='angleIcon' className='ml-[6px]'></img>
       <span className='text-[13px] text-[#1882FF] font-medium ml-[6px]'>STFL17121182045413</span>
     </div>
   )
